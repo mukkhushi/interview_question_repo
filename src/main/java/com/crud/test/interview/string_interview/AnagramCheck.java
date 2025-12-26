d9263f98eea130063c38d0528f5da516ff6bee5f
mukkhushi <mukeshkumarray012@gmail.com>
2025-12-27 00:12:24 +0530

    removed comment

--- src/main/java/com/crud/test/interview/string_interview/AnagramCheck.java
+++ src/main/java/com/crud/test/interview/string_interview/AnagramCheck.java
@@ -8,12 +8,11 @@ public class AnagramCheck {
         String s1 = "Listen";
         String s2 = "saailenbbt";
         String sl1 = Arrays.stream(s1.toLowerCase().split("")).sorted().collect(Collectors.joining());
-        //String sl1 = s1.toLowerCase().chars().sorted().mapToObj(c->(char)c).map(String::valueOf).collect(Collectors.joining());
+        String sl1 = s1.toLowerCase().chars().sorted().mapToObj(c->(char)c).map(String::valueOf).collect(Collectors.joining());
         String sl2 = s2.toLowerCase().chars().sorted().mapToObj(c->(char)c).map(String::valueOf).collect(Collectors.joining());
         if(sl1.equalsIgnoreCase(sl2))
             System.out.println("Anagram");
          else
             System.out.println("Not Anagram");
     }
-
 }
